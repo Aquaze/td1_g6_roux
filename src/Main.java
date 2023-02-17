259d5143bce96a96a2e87460b77c996575e2f275
Aquaze <88437016+Aquaze@users.noreply.github.com>
2023-02-17 09:58:26 +0100

    polynome

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
+import java.net.SocketOption;
 import java.util.Scanner;
-public class Main {
+public class Main{
     public static void main(String[] args) {
+        discriminant();
+    }
+    public static void discriminant() {
+        System.out.println("Quelle est la valeur de a ?");
         Scanner scanner = new Scanner(System.in);
-        int unEntier = scanner.nextInt();
-        float unReel = scanner.nextFloat();
-        System.out.println("J’ai recupere un entier: " + unEntier);
-        System.out.println("J’ai aussi recupere un reel: " + unReel);
+        int a = scanner.nextInt();
+        System.out.println("Quelle est la valeur de b ?");
+        int b = scanner.nextInt();
+        System.out.println("Quelle est la valeur de c ?");
+        int c = scanner.nextInt();
+        int delta = (int) (Math.pow(b, 2) - 4 * a * c);
+        if (delta < 0) {
+            System.out.println("Ce polynome n’a pas de racine reelle");
+        } else if (delta == 0) {
+            System.out.println("Ce polynome a une racine reelle");
+        }else {
+            System.out.println("Ce polynome possède deux racines reelles");
         }
+
     }
 
+}
